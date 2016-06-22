999b762c24b3b7a14bdb18a0c2fef71fb6b9325f
20141105072 <2196690899@qq.com>
2016-06-22 18:09:46 +0800

    calcuator1
    计算器的基本形态已经在视图中呈现，可以进行加减乘除运算，但是还存在一些bug，导致无法进行运算。可以进行数据清零。后续会继续添加其他符号和相应的
    函数，并修改程序消除bug，完善计算器。

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,12 +10,97 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var x: UITextField!
-    @IBOutlet weak var y: UITextField!
-    @IBOutlet weak var z: UITextField!
+    var temp=0.0
+    var flag=0
     @IBAction func add(sender: AnyObject) {
-        var temp=0
-        temp=(Int)(x.text!)!+(Int)(y.text!)!
-        z.text="\(temp)"
+        if flag==0
+        {
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=1
+        }
+        
+    }
+    @IBAction func mul(sender: AnyObject) {
+        if flag==0
+        {
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=2
+        }
+    }
+    @IBAction func sub(sender: AnyObject) {
+        if flag==0
+        {
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=3
+        }
+    }
+    @IBAction func div(sender: AnyObject) {
+        if flag==0
+        {
+            temp=(Double)(x.text!)!
+            x.text=""
+            flag=4
+        }
+    }
+    @IBAction func cal(sender: AnyObject) {
+        switch flag
+            {
+        case 1:
+                temp=temp+(Double)(x.text!)!
+            break
+        case 2:
+            temp=temp-(Double)(x.text!)!
+            break
+        case 3:
+            temp=temp*(Double)(x.text!)!
+            break
+        case 4:
+            if (Double)(x.text!)==0{
+            }
+            else{
+                temp=temp/(Double)(x.text!)!
+            }
+            break
+        default:
+            break
+        }
+        x.text="\(temp)"
+    }
+    @IBAction func clear(sender: AnyObject) {
+        x.text=""
+    }
+    @IBAction func one(sender: AnyObject) {
+        x.text=x.text!+"1"
+    }
+    @IBAction func two(sender: AnyObject) {
+        x.text=x.text!+"2"
+    }
+    @IBAction func three(sender: AnyObject) {
+        x.text=x.text!+"3"
+    }
+    @IBAction func four(sender: AnyObject) {
+        x.text=x.text!+"4"
+    }
+    @IBAction func five(sender: AnyObject) {
+        x.text=x.text!+"5"
+    }
+    @IBAction func six(sender: AnyObject) {
+        x.text=x.text!+"6"
+    }
+    @IBAction func seven(sender: AnyObject) {
+        x.text=x.text!+"7"
+    }
+    @IBAction func eight(sender: AnyObject) {
+        x.text=x.text!+"8"
+    }
+    @IBAction func nine(sender: AnyObject) {
+        x.text=x.text!+"9"
+    }
+    @IBAction func zero(sender: AnyObject) {
+        x.text=x.text!+"0"
     }
 
     override func viewDidLoad() {
@@ -27,6 +112,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
